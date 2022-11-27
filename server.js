8a577f0b16fde63c14cb3e657d6358aff32b0277
Brandon Taft <btaftcan@gmail.com>
2022-11-26 19:56:24 -0500

    eod

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.put('/api/add-image', async (req, res, next) => {
     try {
         const fileStr = req.body.data
         const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-        upload_preset: 'Upload',
+        public_id: 'test',
         })
         let secureURL = uploadResponse.secure_url
         } catch (error) {

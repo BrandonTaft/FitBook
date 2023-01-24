c1d2a1d26cc802811fcaf1d8155732e017edc4d4
Brandon Taft <btaftcan@gmail.com>
2023-01-23 21:08:19 -0500

    deploy

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,35 +3,35 @@ const jwt = require('jsonwebtoken')
 
 function authenticate(req, res, next) {
     // validate the token 
-    let headers = req.headers['authorization']
-    if (headers) {
-        try {
-            const token = headers.split(' ')[1]
-            const decoded = jwt.verify(token, "SECRETKEY")
-            if (decoded) {
-                const id = decoded.id
-                const authUser = models.Users.findOne({
-                    where: {
-                        id: id,
-                    }
-                })
-                if (authUser) {
-                    req.authUser = id
-                    console.log("User Is Authenticated")
-                    next() 
-                } else {
-                    res.json({  success: false, error: 'Unable to authenticate' })
-                    res.redirect('/')
-                }
-            } else {
-                res.json({  success: false, error: 'Unable to authenticate' })
-                res.redirect('/')
-            }
-        } catch { res.json({ success: false, message: 'Not Authenticated' }) }
-    } else {
-        res.json({  success: false, error: 'Required headers are missing...' })
-        res.redirect('/')
-    }
+    // let headers = req.headers['authorization']
+    // if (headers) {
+    //     try {
+    //         const token = headers.split(' ')[1]
+    //         const decoded = jwt.verify(token, "SECRETKEY")
+    //         if (decoded) {
+    //             const id = decoded.id
+    //             const authUser = models.Users.findOne({
+    //                 where: {
+    //                     id: id,
+    //                 }
+    //             })
+    //             if (authUser) {
+    //                 req.authUser = id
+    //                 console.log("User Is Authenticated")
+    //                 next() 
+    //             } else {
+    //                 res.json({  success: false, error: 'Unable to authenticate' })
+    //                 res.redirect('/')
+    //             }
+    //         } else {
+    //             res.json({  success: false, error: 'Unable to authenticate' })
+    //             res.redirect('/')
+    //         }
+    //     } catch { res.json({ success: false, message: 'Not Authenticated' }) }
+    // } else {
+    //     res.json({  success: false, error: 'Required headers are missing...' })
+    //     res.redirect('/')
+    // }
 }
 
 module.exports = authenticate

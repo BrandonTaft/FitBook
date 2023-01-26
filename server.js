de18e689d0c98bdb84b9c2ea5f833563957ba943
Brandon Taft <btaftcan@gmail.com>
2023-01-25 19:16:10 -0500

    deploy

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const FaceBookStrategy = require('passport-facebook').Strategy;
 const authenticate = require('./middlewares/authMiddleware');
 const salt = 10;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://fitbook.surge.sh'
+}));
 require('dotenv').config();
 app.use(express.json({ limit: 52428800 }));
 app.use(express.urlencoded({ extended: true, limit: 52428800 }));
